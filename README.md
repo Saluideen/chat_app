c9b0434a906141eb1cda54c12a3cc9eea799a1ac
ideenkreise <ideenkreise@dell-frappe>
2024-03-21 09:52:35 +0530

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Chat
+
+Chat
+
+#### License
+
+k'
---
+++ chat_app/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Chat",
+			"type": "module",
+			"label": _("Chat")
+		}
+	]
